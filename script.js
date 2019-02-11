d198f58cd10ea9190219d4565ab2c2141f9d05cc
GSarpong4423 <sarpongg@gmail.com>
2019-02-11 14:53:41 +0000

    traversing the DOM

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// $("header nav li:first").css({border: "2px solid red"});
-// $("header nav li:first").css({border: "2px solid red"});
+// Next method
+// $("#content-methods").next().css({border: "3px solid red"});
 
-// first-child and last-child
-// $("#contact ul:first-child").css({border: "2px solid red"});
-// $("#contact ul:last-child").css({border: "2px solid red"});
+// prev method
+// $("#social-nav").prev().css({border: "3px solid blue"});
 
+// // Parent
+// $(".banner-title").parent().css({border: "3px solid pink"});
+
+// // children
+// $("#social-nav").children().({border: "3px solid green"});
+
+// // find
+// $("#contact").find(".facebook").css({border: "3px solid purple"});
