e29babc83285d7498845703f868b728b1aa840a5
GSarpong4423 <sarpongg@gmail.com>
2019-02-11 15:08:15 +0000

    chaining

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-// Next method
-// $("#content-methods").next().css({border: "3px solid red"});
-
-// prev method
-// $("#social-nav").prev().css({border: "3px solid blue"});
-
-// // Parent
-// $(".banner-title").parent().css({border: "3px solid pink"});
-
-// // children
-// $("#social-nav").children().({border: "3px solid green"});
-
-// // find
-// $("#contact").find(".facebook").css({border: "3px solid purple"});
+$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border: "2px solid blue"});
